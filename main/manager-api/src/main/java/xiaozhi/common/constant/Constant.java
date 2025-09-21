54d6abc8ee6b09260be54d4d7e523f19934e159a
hrz <1710360675@qq.com>
2025-09-21 18:07:48 +0800

    Bump to v0.8.3

--- main/manager-api/src/main/java/xiaozhi/common/constant/Constant.java
+++ main/manager-api/src/main/java/xiaozhi/common/constant/Constant.java
@@ -247,7 +247,7 @@ public interface Constant {
     /**
      * 版本号
      */
-    public static final String VERSION = "0.8.1";
+    public static final String VERSION = "0.8.3";
 
     /**
      * 无效固件URL
