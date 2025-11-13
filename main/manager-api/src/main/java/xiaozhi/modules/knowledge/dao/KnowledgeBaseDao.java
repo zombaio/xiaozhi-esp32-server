e6e56122a1e6da5addbe1f8711dc323d5a3f3c5c
rainv123 <2148537152@qq.com>
2025-11-13 12:35:07 +0800

    fix:移除触发器，在DAO层添加了删除方法

--- main/manager-api/src/main/java/xiaozhi/modules/knowledge/dao/KnowledgeBaseDao.java
+++ main/manager-api/src/main/java/xiaozhi/modules/knowledge/dao/KnowledgeBaseDao.java
@@ -1,6 +1,7 @@
 package xiaozhi.modules.knowledge.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import xiaozhi.common.dao.BaseDao;
 import xiaozhi.modules.knowledge.entity.KnowledgeBaseEntity;
@@ -10,5 +11,12 @@ import xiaozhi.modules.knowledge.entity.KnowledgeBaseEntity;
  */
 @Mapper
 public interface KnowledgeBaseDao extends BaseDao<KnowledgeBaseEntity> {
+
+    /**
+     * 根据知识库ID删除相关的插件映射记录
+     * 
+     * @param knowledgeBaseId 知识库ID
+     */
+    void deletePluginMappingByKnowledgeBaseId(@Param("knowledgeBaseId") String knowledgeBaseId);
 
 }
