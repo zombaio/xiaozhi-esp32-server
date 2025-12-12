fc6b8c88171630f76e595d976a4ffdb8158f9e0e
hrz <1710360675@qq.com>
2025-12-12 16:33:50 +0800

    update:更新版本号

--- main/xiaozhi-server/config/logger.py
+++ main/xiaozhi-server/config/logger.py
@@ -5,7 +5,7 @@ from config.config_loader import load_config
 from config.settings import check_config_file
 from datetime import datetime
 
-SERVER_VERSION = "0.8.9"
+SERVER_VERSION = "0.8.10"
 _logger_initialized = False
 
 
